7218e1125c3b95f55f7808a168fc7174c9901203
Camilo Correa <correafuentes.cristian@gmail.com>
2023-06-27 05:42:47 -0500

    Se agrego front de login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from "react";
-import { Header } from "./componentes/Header";
-import { ProductosLista } from "./componentes/Productos/index";
-import 'boxicons';
+import Testimonio  from './components/Testimonio';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Header />
-        <ProductosLista/>
-      </header>
+      <div className='contenedor-principal'>
+          <h1>Testimonios de Nuestro Alumnos</h1>
+          <Testimonio 
+          const jsontext = {'./data/testimonio.json'}
+           nombre='Emma Bostian'
+           pais='suecia'
+           imagen='3'
+           cargo='Ingeniero de Software'
+           empresa='Spotify'
+           testimonio='Lorem ipsum dolor sit amet consectetur 
+                      adipisicing elit. Ut qui temporibus laudantium, 
+                      delectus inventore nihil. Consectetur voluptates 
+                      sapiente, odio modi culpa, inventore nihil maxime alias, 
+                      dolores reprehenderit consequuntur necessitatibus quibusdam!' 
+           titulo='foto1' /> 
+                    
+         <Testimonio 
+           nombre='Shan Wang'
+           pais='Singapur'
+           imagen='2'
+           cargo='Ingeniero de Software'
+           empresa='Amazon'
+           testimonio='Lorem ipsum dolor sit amet consectetur 
+                      adipisicing elit. Ut qui temporibus laudantium, 
+                      delectus inventore nihil. Consectetur voluptates 
+                      sapiente, odio modi culpa, inventore nihil maxime alias, 
+                      dolores reprehenderit consequuntur necessitatibus quibusdam!' 
+           titulo='foto2' /> 
+          <Testimonio 
+           nombre='Sarah'
+           pais='Sudafrica'
+           imagen='1'
+           cargo='Ingeniero de Software'
+           empresa='Realtech'
+           testimonio='Lorem ipsum dolor sit amet consectetur 
+                      adipisicing elit. Ut qui temporibus laudantium, 
+                      delectus inventore nihil. Consectetur voluptates 
+                      sapiente, odio modi culpa, inventore nihil maxime alias, 
+                      dolores reprehenderit consequuntur necessitatibus quibusdam!' 
+           titulo='foto3' />          
+      </div>      
     </div>
   );
 }
