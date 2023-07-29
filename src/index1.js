9da39fcc2ef45f0618fb08417924a3be845c850f
Camilo Correa <correafuentes.cristian@gmail.com>
2023-07-29 05:53:17 -0500

    se agrega funcionalidad de busqueda

--- src/index1.js
+++ src/index1.js
@@ -1,25 +1,13 @@
 const express = require('express');
-const multer = require('multer');
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
-// Configuración de multer para la carga de archivos
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-});
-const upload = multer({ storage });
-
 // Datos simulados en memoria
 const products = [
-    { id: 1, name: 'Producto 1', category: 'Electrónica', images: ['image1.jpg'] },
-    { id: 2, name: 'Producto 2', category: 'Ropa', images: ['image2.jpg'] },
+    { id: 1, name: 'Producto 1', category: 'Electrónica' },
+    { id: 2, name: 'Producto 2', category: 'Ropa' },
     // Agregar más productos aquí
 ];
 
@@ -27,26 +15,20 @@ app.use(bodyParser.json());
 
 // Ruta para búsqueda avanzada
 app.get('/search', (req, res) => {
-    // ... (código de búsqueda previo)
-});
+    const { category, keyword } = req.query;
+    let results = products;
 
-// Ruta para agregar imágenes a un producto
-app.post('/add-image/:productId', upload.array('images', 5), (req, res) => {
-    const productId = parseInt(req.params.productId);
-    const product = products.find(p => p.id === productId);
-
-    if (!product) {
-        return res.status(404).json({ message: 'Producto no encontrado' });
+    if (category) {
+        results = results.filter(product => product.category === category);
     }
 
-    if (req.files && req.files.length > 0) {
-        req.files.forEach(file => {
-            product.images.push(file.filename);
-        });
-        return res.json({ message: 'Imágenes agregadas con éxito' });
+    if (keyword) {
+        results = results.filter(product =>
+            product.name.toLowerCase().includes(keyword.toLowerCase())
+        );
     }
 
-    return res.status(400).json({ message: 'No se encontraron imágenes para cargar' });
+    res.json(results);
 });
 
 // Iniciar el servidor
