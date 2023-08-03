0ff9fc4aba4c9f94fe05dfa1183b14dd49228bf6
santiago982 <63021557+santiago982@users.noreply.github.com>
2023-08-02 21:16:09 -0500

    creación página usuarios,configuracion proyecto

---
+++ trueque_app_frontend- Create users page, config project/components/userForm.tsx
@@ -0,0 +1,36 @@
+// components/UserForm.tsx
+import React, { useState } from 'react';
+
+const UserForm = ({ onSubmit }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ username, password });
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+        required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        required
+      />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default UserForm;
---
+++ trueque_app_frontend- Create users page, config project/src/app/users/login/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+// pages/login.tsx
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Aquí puedes implementar la lógica de validación y autenticación con el backend
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
