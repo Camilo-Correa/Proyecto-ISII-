2c41a411d7ed6e034fc2635a39180454fecd424c
santiago982 <63021557+santiago982@users.noreply.github.com>
2023-08-10 15:29:08 -0500

    Login Movido Ruta Externa

---
+++ trueque_app_frontend-LoginMovidoRutaExterna/components/userList.tsx
@@ -0,0 +1,17 @@
+// components/UserList.tsx
+import React from 'react';
+import Link from 'next/link';
+
+const UserList = ({ users }) => {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link href={`/users/${user.id}`}>{user.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
