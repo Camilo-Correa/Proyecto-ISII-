c9e4349090326a7e97388b95e3f52a22bd28fbdf
santiago982 <63021557+santiago982@users.noreply.github.com>
2023-09-09 21:54:17 -0500

    trueque_app_frontend- lógica de login,
    logout y listado de usuarios creada, implementacion auth

---
+++ trueque_app_frontend-582f38d57121951b888a3049db0e8ba928d24d42/src/app/auth.api.ts
@@ -0,0 +1,17 @@
+export async function PUTloginUser(name: string, password: string) {
+  try {
+    const resp = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/login`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, password }),
+      }
+    );
+    return resp;
+  } catch (error) {
+    console.log(error);
+  }
+}
