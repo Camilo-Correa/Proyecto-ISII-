c81f43926d68a6dc3ad3094cbd7ca31f808c71b2
santiago982 <63021557+santiago982@users.noreply.github.com>
2023-09-12 10:06:10 -0500

    Delete trueque_app_frontend-lógica de login, logout y listado de usuarios creada, implementacion directory
    carga archivo equivocado

--- trueque_app_frontend-lógica de login, logout y listado de usuarios creada, implementacion/model/user.model.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  code: string;
-  role: string;
-  name: string;
-  email: string;
-  created_at: Date;
-  updated_at: Date;
-}
--- trueque_app_frontend-lógica de login, logout y listado de usuarios creada, implementacion/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  },
-};
-
-module.exports = nextConfig;
