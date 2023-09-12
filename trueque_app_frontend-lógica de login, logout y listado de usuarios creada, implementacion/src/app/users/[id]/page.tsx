0156e36307f5d1f77ef34505f87b8dc9a0b8743b
santiago982 <63021557+santiago982@users.noreply.github.com>
2023-09-12 09:50:55 -0500

    trueque_app_frontend-lógica de login, logout
    y listado de usuarios creada, implementación

---
+++ trueque_app_frontend-lógica de login, logout y listado de usuarios creada, implementacion/src/app/users/[id]/page.tsx
@@ -0,0 +1,22 @@
+// pages/users/[id].tsx
+import React from 'react';
+import Link from 'next/link';
+import usersData from '../../data/users.json';
+
+const UserDetail = ({ params }) => {
+  const user = getUserById(params.id);
+  return (
+    <div>
+      <h1>User Detail</h1>
+      <p>nombre: {user.name}</p>
+      <Link href="/users">Back to User List</Link>
+    </div>
+  );
+};
+
+function getUserById(id) {
+  const user = usersData.find((user) => user.id === parseInt(id));
+  return user;
+}
+
+export default UserDetail;
